09ffc31ce9740d71afb24ff6916d5b00ca4ea00f
Kevin <your.email@address.com>
2020-08-26 22:32:16 -0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import EmojiObjectsIcon from "@material-ui/icons/EmojiObjects";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EmojiObjectsIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
